06bc4db8fe0e63b6649f14cde86e60b95d9c26d1
johnsani <sanijohn2x@gmail.com>
2016-02-18 08:30:01 +0100

    A funtion using TDD approach to test a two_sum function

--- exercises/two_sum_test.py
+++ exercises/two_sum_test.py
@@ -19,5 +19,29 @@ class TwoSumTestSuite(unittest.TestCase):
 		result = two_sum([1, 10, 10, 8, 6, 5, 4], 12)
 		self.assertEqual(result, [3, 6])
 
+	def test_list_range_8(self):
+		result = two_sum([1, 10, 10, 8, 6, 5, 4, 56], 60)
+		self.assertEqual(result, [6, 7])
+
+	def test_list_range_9(self):
+		result = two_sum([100, 50, 70, 80, 6, 5, 4, 2, 5], 150)
+		self.assertEqual(result, [0, 1])
+
+	def test_list_range_10(self):
+		result = two_sum([4, 10, 13, 8, 6, 5, 4, 23, 34, 88], 57)
+		self.assertEqual(result, [7, 8])
+
+	def test_list_range_2(self):
+		result = two_sum([1, 10], 11)
+		self.assertEqual(result, [0, 1])
+
+	def test_list_range_3(self):
+		result = two_sum([5, 55, 60], 65)
+		self.assertEqual(result, [0, 2])
+
+	def test_list_range_12(self):
+		result = two_sum([17, 10, 18, 7, 65, 25, 40], 24)
+		self.assertEqual(result, [0, 3])
+
 if __name__ == '__main__':
 	unittest.main()
