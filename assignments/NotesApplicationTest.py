75d1f2590294f92d9fc963e8b57e1efae86749ac
johnsani <sanijohn2x@gmail.com>
2016-02-22 12:20:21 +0100

    Test cases for Note Taking application

--- assignments/NotesApplicationTest.py
+++ assignments/NotesApplicationTest.py
@@ -14,11 +14,22 @@ class NotesApplicationTestSuite(unittest.TestCase):
 
     def test_NotesApplication_search(self):
         search = NotesApplication('John')
-        self.assertEqual("Not found", search.search(" lime"))
+        self.assertEqual("Not found", search.search("lists"))
 
     def test_NotesApplication_list(self):
         lists = NotesApplication('John')
-        self.assertEqual("Sorry list is empty!", search.lists("Nice"))
+        self.assertEqual("Sorry list is empty!", lists.lists())
+
+    def test_NotesApplication_list2(self):
+        list2 = NotesApplication('John')
+        counter = list2.list()
+        self.assertEqual(counter, 2)
+
+    def test_NotesApplication_list3(self):
+        list3 = NotesApplication('John')
+        counter = list3.list()
+        self.assertEqual(counter, 3)
+
 
     def test_NotesApplication_get(self):
         get = NotesApplication('John')
@@ -29,6 +40,11 @@ class NotesApplicationTestSuite(unittest.TestCase):
         self.assertEqual("Sorry wrong ID! View the list and select the right ID!", delete2.delete(7))
 
 
+    def test_NotesApplication_delete3(self):
+        delete3 = NotesApplication('John')
+        self.assertEqual("Sorry list is empty!", delete3.delete(3))
+
+
     def test_NotesApplication_delete(self):
         delete = NotesApplication('John')
         self.assertEqual("Note successfully deleted!", delete.delete(0))
@@ -36,6 +52,11 @@ class NotesApplicationTestSuite(unittest.TestCase):
     def test_NotesApplication_edit(self):
         edit = NotesApplication('John')
         self.assertEqual("Noted edited successfully!", edit.edit(0, "Wonderful!"))
+
+    def test_NotesApplication_edit2(self):
+        edit2 = NotesApplication('John')
+        self.assertEqual("Sorry list is empty!", edit2.edit(3, "Great"))
+
 
 
 if __name__ == '__main__':
